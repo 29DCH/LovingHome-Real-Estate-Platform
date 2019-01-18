7156d0ac1397964556ad8c38b01cde1a9ec85cdc
LJQ <2265940560@qq.com>
2019-01-18 11:36:17 +0800

    Add files via upload

---
+++ springboot-pay-example/src/main/java/com/example/pay/configuration/WebMvcConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.pay.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * WebMVC 配置.
+ * <p>
+ * 添加路径和页面的映射关系
+ *
+ */
+@Configuration
+public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+    @Override
+    protected void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/gotoWapPage").setViewName("gotoWapPay");
+        registry.addViewController("/gotoPagePage").setViewName("gotoPagePay");
+        super.addViewControllers(registry);
+    }
+}
